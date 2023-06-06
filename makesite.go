5523f2812fc78c12d39f94b588a0ac6e1af9316a
Elias <e.wambugu192@gmail.com>
2023-06-06 14:41:17 -0700

    generate html file from template and provided txt file from command line

--- makesite.go
+++ makesite.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"html/template"
+	"io/ioutil"
+	"os"
+	// "fmt"
+)
+
+type Page struct {
+	Contents string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	filePath := flag.String("file", "", "Get file from command-line input")
+	flag.Parse()
+
+	fileContents, fileContentsErr := ioutil.ReadFile(*filePath)
+
+	if fileContentsErr != nil {
+		panic(fileContentsErr)
+	}
+
+	page := Page{
+		Contents: string(fileContents),
+	}
+
+	t := template.Must(template.ParseFiles("template.tmpl"))
+
+	newFile, err := os.Create("new.html")
+
+	if err != nil {
+		panic(err)
+	}
+
+	t.Execute(newFile, page)
+	// t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	// newFile, err := os.Create("new.html")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// t.Execute(newFile, page)
 }
